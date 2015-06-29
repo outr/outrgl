6b8977ad3e8575401d04d946864c7f24772826c6
Matt Hicks <matt@outr.com>
2015-06-29 11:00:19 -0500

    Created FocusSupport trait and updated TextInput to handle placeholder hiding and showing based on content in the field.

--- core/src/main/scala/com/outr/gl/actor/TextInput.scala
+++ core/src/main/scala/com/outr/gl/actor/TextInput.scala
@@ -1,11 +1,16 @@
 package com.outr.gl.actor
 
-import com.badlogic.gdx.scenes.scene2d.ui.TextField.TextFieldStyle
+import com.badlogic.gdx.scenes.scene2d.ui.TextField.{TextFieldListener, TextFieldStyle}
 import com.badlogic.gdx.scenes.scene2d.ui.{Label, TextField}
+import com.outr.gl.actor.addon.FocusSupport
 
 /**
  * @author Matt Hicks <matt@outr.com>
  */
-class TextInput(text: String, placeholder: Label, style: TextFieldStyle) extends TextField(text, style) {
+class TextInput(placeholder: Label, style: TextFieldStyle) extends TextField("", style) with FocusSupport {
+  setTextFieldListener(new TextFieldListener {
+    override def keyTyped(textField: TextField, c: Char) = updatePlaceholder()
+  })
 
+  private def updatePlaceholder() = placeholder.setVisible(getText.isEmpty)
 }
---
+++ core/src/main/scala/com/outr/gl/actor/addon/FocusSupport.scala
@@ -0,0 +1,27 @@
+package com.outr.gl.actor.addon
+
+import com.badlogic.gdx.scenes.scene2d.Actor
+import com.badlogic.gdx.scenes.scene2d.utils.FocusListener
+import com.badlogic.gdx.scenes.scene2d.utils.FocusListener.FocusEvent
+import org.powerscala.property.Property
+
+/**
+ * @author Matt Hicks <matt@outr.com>
+ */
+trait FocusSupport {
+  this: Actor =>
+
+  private val _focused = Property[Boolean](default = Some(false))
+  val focused = _focused.readOnlyView
+  def hasFocus = focused()
+
+  addListener(new FocusListener {
+    override def keyboardFocusChanged(event: FocusEvent, actor: Actor, focused: Boolean) = {
+      _focused := focused
+    }
+
+    override def scrollFocusChanged(event: FocusEvent, actor: Actor, focused: Boolean) = {
+      _focused := focused
+    }
+  })
+}
