655fbdec3df0822155ffb21db5647cac23cbe0bb
Matt Hicks <matt@outr.com>
2016-01-04 14:06:46 -0600

    Defaulting SpriteSheetCreator to generate pot textures.

--- tools/src/main/scala/com/outr/gl/tools/SpriteSheetCreator.scala
+++ tools/src/main/scala/com/outr/gl/tools/SpriteSheetCreator.scala
@@ -10,12 +10,12 @@ import com.badlogic.gdx.tools.texturepacker.TexturePacker.Settings
  * @author Matt Hicks <matt@outr.com>
  */
 object SpriteSheetCreator {
-  def generate(in: File, out: File, outName: String = "sprites", mipMap: Boolean = true) = {
+  def generate(in: File, out: File, outName: String = "sprites", mipMap: Boolean = true, pot: Boolean = true) = {
     val settings = new Settings()
     settings.maxWidth = 2048
     settings.maxHeight = 2048
     settings.combineSubdirectories = true
-    settings.pot = false
+    settings.pot = pot
     settings.filterMin = if (mipMap) TextureFilter.MipMapLinearLinear else TextureFilter.Linear
     settings.filterMag = TextureFilter.Linear
     TexturePacker.process(settings, in.getCanonicalPath, out.getCanonicalPath, outName)
