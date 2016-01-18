a984f8708d182d33826065d1c8184b3de82b27b8
Matt Hicks <matt@outr.com>
2016-01-18 08:58:39 -0600

    Added PortraitOnly mix-in.

---
+++ core/src/main/scala/com/outr/gl/screen/PortraitOnly.scala
@@ -0,0 +1,19 @@
+package com.outr.gl.screen
+
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.Input.Orientation
+
+trait PortraitOnly extends BaseScreen {
+  override def render(delta: Float): Unit = {
+    super.render(delta)
+
+    if (MultiScreenApplication().orientation() == Orientation.Landscape) {
+      Gdx.input.setOnscreenKeyboardVisible(false)
+      stage.getRoot.setRotation(90.0f)
+      stage.getRoot.setPosition(Gdx.graphics.getWidth, 0.0f)
+    } else {
+      stage.getRoot.setRotation(0.0f)
+      stage.getRoot.setPosition(0.0f, 0.0f)
+    }
+  }
+}
