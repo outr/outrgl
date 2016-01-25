e5a4cb01d76159043bbee91545f06efc27a8b876
Matt Hicks <matt@outr.com>
2016-01-25 09:48:11 -0600

    Temporarily disabled PortraitOnly functionality.

--- core/src/main/scala/com/outr/gl/screen/PortraitOnly.scala
+++ core/src/main/scala/com/outr/gl/screen/PortraitOnly.scala
@@ -4,7 +4,7 @@ import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.Input.Orientation
 
 trait PortraitOnly extends BaseScreen {
-  override def render(delta: Float): Unit = {
+  /*override def render(delta: Float): Unit = {
     super.render(delta)
 
     if (MultiScreenApplication().orientation() == Orientation.Landscape) {
@@ -15,5 +15,5 @@ trait PortraitOnly extends BaseScreen {
       stage.getRoot.setRotation(0.0f)
       stage.getRoot.setPosition(0.0f, 0.0f)
     }
-  }
+  }*/
 }
