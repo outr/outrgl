9bb8802bc668515b683d5d62fe85d032268564fe
Matt Hicks <matt@outr.com>
2016-01-27 07:54:29 -0600

    Re-enabled PortraitOnly.

--- core/src/main/scala/com/outr/gl/screen/PortraitOnly.scala
+++ core/src/main/scala/com/outr/gl/screen/PortraitOnly.scala
@@ -4,16 +4,15 @@ import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.Input.Orientation
 
 trait PortraitOnly extends BaseScreen {
-  /*override def render(delta: Float): Unit = {
+  override def render(delta: Float): Unit = {
     super.render(delta)
 
     if (MultiScreenApplication().orientation() == Orientation.Landscape) {
-      Gdx.input.setOnscreenKeyboardVisible(false)
       stage.getRoot.setRotation(90.0f)
       stage.getRoot.setPosition(Gdx.graphics.getWidth, 0.0f)
     } else {
       stage.getRoot.setRotation(0.0f)
       stage.getRoot.setPosition(0.0f, 0.0f)
     }
-  }*/
+  }
 }
